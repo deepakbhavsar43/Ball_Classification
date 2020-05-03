a07ad6308d3bd72496053c1bc509915bb0f4878b
deepakbhavsar43 <deepakbhavsar43@gmail.com>
2020-05-03 18:31:28 +0530

    Add: Added option to save trained model

--- Custom.py
+++ Custom.py
@@ -1,52 +1,164 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-import time
+training_data = [
+        [35,"Rough","Tennis"],
+        [47,"Rough","Tennis"],
+        [90,"Smooth","Cricket"],
+        [48,"Rough","Tennis"],
+        [90,"Smooth","Cricket"],
+        [35,"Rough","Tennis"],
+        [92,"Smooth","Cricket"],
+        [35,"Rough","Tennis"],
+        [35,"Rough","Tennis"],
+        [35,"Rough","Tennis"],
+        [96,"Smooth","Cricket"],
+        [43,"Rough","Tennis"],
+    ]
 
-st = time.time()
-print(st)
-st = time.time()
-print(st)
+# Column labels.
+header = ["weight", "surface", "label"]
 
-exit()
-dataset = pd.read_csv("Dataset/Ball_Dataset.csv")
-dataset["Surface"] = pd.factorize(dataset.Surface)[0]
-dataset["Label"] = pd.factorize(dataset.Label)[0]
-X = dataset.drop(["Label"], axis=1)
-Y = dataset["Label"]
 
-# Converted textual data into numbers manually
-# 0 = Rough, Tennis
-# 1 = Smooth, Cricket
-# X = [[35, 0],
-#      [47, 0],
-#      [90, 1],
-#      [48, 0],
-#      [90, 1],
-#      [35, 0],
-#      [92, 1],
-#      [35, 0],
-#      [35, 0],
-#      [35, 0],
-#      [96, 1],
-#      [43, 0],
-#      [110, 1],
-#      [35, 0],
-#      [95, 1]]
-# y = [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1]
+def unique_vals(rows, col):
+    return set([row[col] for row in rows])
 
-# Splitting the data for training and testing
-xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-# Creating object of decision tree
-dtc = DecisionTreeClassifier()
+def class_counts(rows):
+    counts = {}
+    for row in rows:
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+    return counts
 
-# Fitting the training data
-training = dtc.fit(xTrain, yTrain)
 
-# predicting
-yPredict = dtc.predict(xTest)
+def is_numeric(value):
+    return isinstance(value, int) or isinstance(value, float)
 
-# Displaying the result
-print("Input ta given to model :\n", xTest)
-print("Predicted label by the model :\n", yPredict, end="\n")
+class Question:
+
+    def __init__(self, column, value):
+        self.column = column
+        self.value = value
+
+    def match(self, example):
+        val = example[self.column]
+        if is_numeric(val):
+            return val >= self.value
+        else:
+            return val == self.value
+
+    def __repr__(self):
+        condition = "=="
+        if is_numeric(self.value):
+            condition = ">="
+        return "Is %s %s %s?" % (
+            header[self.column], condition, str(self.value))
+
+
+def partition(rows, question):
+    true_rows, false_rows = [], []
+    for row in rows:
+        if question.match(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+    return true_rows, false_rows
+
+def gini(rows):
+    counts = class_counts(rows)
+    impurity = 1
+    for lbl in counts:
+        prob_of_lbl = counts[lbl] / float(len(rows))
+        impurity -= prob_of_lbl ** 2
+    return impurity
+
+
+def info_gain(left, right, current_uncertainty):
+    p = float(len(left)) / (len(left) + len(right))
+    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+
+
+def find_best_split(rows):
+    best_gain = 0
+    best_question = None
+    current_uncertainty = gini(rows)
+    n_features = len(rows[0]) - 1
+
+    for col in range(n_features):
+
+        values = set([row[col] for row in rows])
+
+        for val in values:
+
+            question = Question(col, val)
+
+            true_rows, false_rows = partition(rows, question)
+
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+
+            gain = info_gain(true_rows, false_rows, current_uncertainty)
+
+            if gain >= best_gain:
+                best_gain, best_question = gain, question
+
+    return best_gain, best_question
+
+class Leaf:
+    def __init__(self, rows):
+        self.predictions = class_counts(rows)
+
+
+class Decision_Node:
+    def __init__(self,
+                 question,
+                 true_branch,
+                 false_branch):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+
+def build_tree(rows):
+    gain, question = find_best_split(rows)
+    if gain == 0:
+        return Leaf(rows)
+    true_rows, false_rows = partition(rows, question)
+
+    true_branch = build_tree(true_rows)
+
+    false_branch = build_tree(false_rows)
+
+    return Decision_Node(question, true_branch, false_branch)
+
+def classify(row, node):
+    if isinstance(node, Leaf):
+        return node.predictions
+
+    if node.question.match(row):
+        return classify(row, node.true_branch)
+    else:
+        return classify(row, node.false_branch)
+
+
+def print_leaf(counts):
+    total = sum(counts.values()) * 1.0
+    probs = {}
+    for lbl in counts.keys():
+        probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
+    return probs
+
+
+if __name__ == '__main__':
+
+    my_tree = build_tree(training_data)
+
+    testing_data = [
+        [110,"Smooth","Cricket"],
+        [35,"Rough","Tennis"],
+        [95,"Smooth","Cricket"],
+    ]
+
+    for row in testing_data:
+        print("Actual: %s. Predicted: %s" %
+              (row[-1], print_leaf(classify(row, my_tree))))
